4a38b1a96c3d08fdb83503fff68d8bdb64720b7c
susthir <susthir19march@gmail.com>
2024-05-07 13:06:40 +0530

    dark light mode

--- learning/src/App.jsx
+++ learning/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import { Navbar } from './components/Navbar'
 import TextArea from './components/TextArea'
 import About from './components/About'
 
 
 function App() {
-  
+  const[mode,setMode] =useState('light');
+
+  const togglemode =()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor='grey'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+    }
+  }
 
   return (
     <>
-      <Navbar/>
+      <Navbar  mode={mode} togglemode={togglemode} />
       <div className="container my-4 ">
       <About/>
-      <TextArea heading="Enter text to analyze" />
+      <TextArea heading="Enter text to analyze" togglemode={togglemode}/>
       </div>
       
     </>
